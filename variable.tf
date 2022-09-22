eeef0ef5d2036df774f95e7860c1eb42c57f0cd4
swathidevops123 <109300978+swathidevops123@users.noreply.github.com>
2022-09-22 15:04:49 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 	region		="ap-south-1"
-	access_key		="AKIATMELVKS733YAFZMT"
-	secret_key		="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"
+	access_key		="AKIA4KLRXXYEBZ2OE2NZ"
+	secret_key		="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"
 	}
 
